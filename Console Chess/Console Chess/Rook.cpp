bd61af7a5d22588ccbe9cc3a99de3f52adafc4ef
IceUniverse6 <ali84azizi2005@gmail.com>
2025-06-29 16:09:11 +0330

    class Rook

---
+++ Console Chess/Console Chess/Rook.cpp
@@ -0,0 +1,46 @@
+﻿#include "Rook.h"
+
+Rook::Rook(bool isWhite) : Piece(isWhite), isMoved(false) {}
+
+bool Rook::canMove(const Position& from, const Position& to, Piece* board[8][8]) const {
+    int fromRow = from.getRow();
+    int fromCol = from.getColumn();
+    int toRow = to.getRow();
+    int toCol = to.getColumn();
+
+    if (fromRow == toRow && fromCol != toCol) {
+        int step = (toCol > fromCol) ? 1 : -1;
+        for (int col = fromCol + step; col != toCol; col += step) {
+            if (board[fromRow][col] != nullptr) {
+                return false;
+            }
+        }
+    }
+
+    else if (fromCol == toCol && fromRow != toRow) {
+        int step = (toRow > fromRow) ? 1 : -1;
+        for (int row = fromRow + step; row != toRow; row += step) {
+            if (board[row][fromCol] != nullptr) {
+                return false;
+            }
+        }
+    }
+    else {
+        return false;
+    }
+
+    Piece* target = board[toRow][toCol];
+    return (target == nullptr) || (target->isWhitePiece() != isWhitePiece());
+}
+
+std::wstring Rook::getSymbol() const {
+    return isWhite ? L"♜" : L"♖";
+}
+
+void Rook::moved() {
+    isMoved = true;
+}
+
+bool Rook::hasMoved() const {
+    return isMoved;
+}
